69c494b2f1875578aeeb962bba2dd8bfe8be770b
DorinDavidov <Dorinyme@gmail.com>
2018-12-25 23:39:06 +0200

    unfinishede

---
+++ Var.sh
@@ -0,0 +1,8 @@
+#############################
+# Made by: Dorin
+# Date: 25.12.18
+# Purpose: echo variables
+# Version: 0.0.1
+#############################
+
+echo "{Home: $HOME}" "{User: $USER}" "{Hostname: $HOSTNAME}" "{Display: $DISPLAY}" "{Term: $TERM}" "{Path: $PATH}" "{Histsize: $HISTSIZE}" "{Session_Manager: $SESSION_MANAGER}" "{Shell: $SHELL}" "{Lang: $LANG}" "{Mail: $MAIL Dorinyme@gmail.com}"
