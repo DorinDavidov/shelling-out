2178181a7576ac230c4ead9dd6a7f8c6973ecbc8
DorinDavidov <Dorinyme@gmail.com>
2018-12-26 12:41:36 +0200

    Done

---
+++ Iam.sh
@@ -0,0 +1,13 @@
+#############################
+# Made by: Dorin
+# Date: 26.12.18
+# Purpose: who an I
+# Version: 0.0.1
+#############################
+
+printf "%s\n" $fname
+printf "%s\n" $lname
+printf "%s\n" $uname
+printf "%s\n" $dob
+printf "%s\n" $mail
+
