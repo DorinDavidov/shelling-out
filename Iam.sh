2a6d8a761582b53c9334826ff6628f7fd8d3bfd8
DorinDavidov <Dorinyme@gmail.com>
2019-01-11 12:29:22 +0200

    small changes

--- Iam.sh
+++ Iam.sh
@@ -4,13 +4,15 @@
 # Made by: Dorin
 # Date: 26.12.18
 # Purpose: Training
-# Version: 0.0.1
+# Version: 0.0.2
 #############################
+
 fname="Dorin"
 lname="Davidov"
 uname="Dorinyme"
 dob="26.10.1995"
 mail="dorinyme@gmail.com"
+
 printf "%s\n" $fname
 printf "%s\n" $lname
 printf "%s\n" $uname
